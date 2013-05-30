ad36a502eccca10dfbcfb79cd599be8b7b74939e
Ben-Hur Carlos Vieira Langoni Junior <bhlangonijr@gmail.com>
2013-05-30 15:36:04 -0300

    [VOICE-98] Fixed SID atn ChargeServiceProcessor#createServiceRequest

--- src/main/java/org/jinglenodes/credit/ChargeServiceProcessor.java
+++ src/main/java/org/jinglenodes/credit/ChargeServiceProcessor.java
@@ -82,7 +82,7 @@ public class ChargeServiceProcessor extends AbstractServiceProcessor {
                     e.addAttribute("initiator", from.toBareJID());
                     e.addAttribute("responder", toBareJid);
                     e.addAttribute("seconds", String.valueOf(callTime));
-                    e.addAttribute("sid", session.getId());
+                    e.addAttribute("sid", jingleIQ.getJingle().getSid());
                     request.setChildElement(e);
                     log.debug("createdCreditRequest: " + request.toXML());
                     credit.setCharged(true);
